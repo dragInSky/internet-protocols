80f598cf99465cf3034bd81eee6b854a01be333b
draginsky <cronosdversus@mail.ru>
2023-05-08 17:25:40 +0500

    vkApi update

--- vkApi/README.md
+++ vkApi/README.md
@@ -3,11 +3,11 @@
 
 ### Описание:
 Использование vkApi. Вывод в удобочитаемом виде информации о пользователе по его id в vk:
-списка друзей, списка подписчиков, списка подписок
+списки друзей, подписчиков, подписок и групп
 
 ### Usage:
 ```
-main.py [-h] [-i USERID] [-f] [-l] [-s]
+main.py [-h] [-i USERID] [-f] [-l] [-s] [-g] [-p]
 ```
 
 ### Optional arguments:
@@ -15,10 +15,12 @@ main.py [-h] [-i USERID] [-f] [-l] [-s]
   -h, --help            show this help message and exit
   -i USERID, --userId USERID
                         target user id
-  -f, --friends         friends pretty print
-  -l, --followers       followers pretty print
-  -s, --subscriptions   subscriptions pretty print
+  -f, --friends         friends print
+  -l, --followers       followers print
+  -s, --subscriptions   subscriptions print
+  -g, --groups          groups print
+  -p, --photos          groups print
 ```
 
 ### Демонстрация программы:
-...
+![](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzhmMjk0M2E1MWIzMmY0YzVmM2M5YmM2NzNjMDA1NmZmNGVkYmMwYiZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/ha6Eke1yUxzcIaERhI/giphy.gif)
--- vkApi/main.py
+++ vkApi/main.py
@@ -11,49 +11,69 @@ def get_token():
     return config['vk_api']['access_token']
 
 
-session = vk_api.VkApi(token=get_token())
-vk = session.get_api()
-
-
-def pretty_items_print(items_info, first_name='first_name', last_name='last_name'):
+def pretty_items_print(items_info, second_column, third_column):
     output = list(list())
-    for user_info in items_info:
+    for item_info in items_info:
         row = [
-            user_info['id'],
-            user_info[first_name],
-            user_info[last_name],
+            item_info['id'],
+            item_info[second_column],
+            item_info[third_column],
         ]
         output.append(row)
-    print(tabulate(output, headers=['id', first_name, last_name]))
+    print(tabulate(output, headers=['id', second_column, third_column]))
 
 
-def items_list_to_str(items):
-    str_users = ''
-    for user in items['items']:
-        str_users += str(user) + ','
-    return str_users.removesuffix(',')
+class ApiVk:
+    def __init__(self, user_id):
+        self._session = vk_api.VkApi(token=get_token())
+        self._user_id = user_id
 
+    @staticmethod
+    def _items_list_to_str(items):
+        str_items = ''
+        for item in items['items']:
+            str_items += str(item) + ','
+        return str_items.removesuffix(',')
 
-def print_users(user_id, method_name, title):
-    users = session.method(method_name, {"user_id": user_id})
+    def print_friends(self):
+        friends = self._session.method("friends.get", {"user_id": self._user_id})
 
-    str_users = items_list_to_str(users)
+        str_friends = self._items_list_to_str(friends)
 
-    users_info = session.method("users.get", {"user_ids": str_users})
+        friends_info = self._session.method("users.get", {"user_ids": str_friends})
 
-    print(title)
-    pretty_items_print(users_info, first_name='first_name', last_name='last_name')
+        print("Friends list:")
+        pretty_items_print(friends_info, second_column='first_name', third_column='last_name')
 
+    def print_followers(self):
+        followers = self._session.method("users.getFollowers", {"user_id": self._user_id})
 
-def print_subscriptions(user_id):
-    subscriptions = session.method("users.getSubscriptions", {"user_id": user_id})
+        str_followers = self._items_list_to_str(followers)
 
-    group_ids = items_list_to_str(subscriptions['groups'])
+        followers_info = self._session.method("users.get", {"user_ids": str_followers})
 
-    subscriptions_info = session.method("groups.getById", {"group_ids": group_ids})
+        print("Followers list:")
+        pretty_items_print(followers_info, second_column='first_name', third_column='last_name')
 
-    print("Subscriptions list:")
-    pretty_items_print(subscriptions_info, first_name='name', last_name='screen_name')
+    def print_subscriptions(self):
+        subscriptions = self._session.method("users.getSubscriptions", {"user_id": self._user_id})
+
+        group_ids = self._items_list_to_str(subscriptions['groups'])
+
+        subscriptions_info = self._session.method("groups.getById", {"group_ids": group_ids})
+
+        print("Subscriptions list:")
+        pretty_items_print(subscriptions_info, second_column='name', third_column='screen_name')
+
+    def print_groups(self):
+        groups = self._session.method("groups.get", {"user_id": self._user_id})
+
+        group_ids = self._items_list_to_str(groups)
+
+        groups_info = self._session.method("groups.getById", {"group_ids": group_ids})
+
+        print("Groups list:")
+        pretty_items_print(groups_info, second_column='name', third_column='screen_name')
 
 
 def main():
@@ -69,28 +89,37 @@ def main():
         '-f',
         '--friends',
         action="store_true",
-        help='friends pretty print'
+        help='friends print'
     )
     parser.add_argument(
         '-l',
         '--followers',
         action="store_true",
-        help='followers pretty print'
+        help='followers print'
     )
     parser.add_argument(
         '-s',
         '--subscriptions',
         action="store_true",
-        help='subscriptions pretty print'
+        help='subscriptions print'
+    )
+    parser.add_argument(
+        '-g',
+        '--groups',
+        action="store_true",
+        help='groups print'
     )
     args = parser.parse_args()
 
+    api_vk = ApiVk(args.userId)
     if args.friends:
-        print_users(args.userId, method_name="friends.get", title="Friends list:")
+        api_vk.print_friends()
     if args.followers:
-        print_users(args.userId, method_name="users.getFollowers", title="Followers list:")
+        api_vk.print_followers()
     if args.subscriptions:
-        print_subscriptions(args.userId)
+        api_vk.print_subscriptions()
+    if args.groups:
+        api_vk.print_groups()
 
 
 if __name__ == '__main__':
